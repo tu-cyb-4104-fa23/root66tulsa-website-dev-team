b121117e0501fc6b8b382e5abe70a7c2e91e6d9f
Codi West <codiwest@utulsa.edu>
2023-11-15 10:55:19 -0600

    Adds Template Assignment

--- root66_website/front_end/urls.py
+++ root66_website/front_end/urls.py
@@ -5,23 +5,22 @@ from django.views.generic import TemplateView
 app_name = 'front_end'
 
 urlpatterns = [
-    path("", views.home, name='home'),
+    path("", views.home, name='home'), # Eli
     # Class-based TemplateViews: https://docs.djangoproject.com/en/4.2/topics/class-based-views/
     path("resources/", TemplateView.as_view(template_name="front_end/resources.html"), name='resources'),
-    path("about/", TemplateView.as_view(template_name="front_end/about.html"), name='about'),
-    path("contact/", TemplateView.as_view(template_name="front_end/contact.html"), name='contact'),
-    path("about/", TemplateView.as_view(template_name="front_end/about.html"), name='about'),
-    path("sponsors/", TemplateView.as_view(template_name="front_end/sponsors.html"), name='sponsors'),
+    path("contact/", TemplateView.as_view(template_name="front_end/contact.html"), name='contact'), # Sean
+    path("about/", TemplateView.as_view(template_name="front_end/about.html"), name='about'), # Sean
+    path("sponsors/", TemplateView.as_view(template_name="front_end/sponsors.html"), name='sponsors'), # Julian
 
 
-    path("alumni/", TemplateView.as_view(template_name="front_end/alumni.html"), name='alumni'),
-    path("clubs/", TemplateView.as_view(template_name="front_end/clubs.html"), name='clubs'),
-    path("clubs/wicys", TemplateView.as_view(template_name="front_end/wicys.html"), name='wicys'),
-    path("clubs/ctf", TemplateView.as_view(template_name="front_end/ctf.html"), name='ctf'),
+    path("alumni/", TemplateView.as_view(template_name="front_end/alumni.html"), name='alumni'), # Devan
+    path("clubs/", TemplateView.as_view(template_name="front_end/clubs.html"), name='clubs'), # Ben
+    path("clubs/wicys", TemplateView.as_view(template_name="front_end/wicys.html"), name='wicys'), # Rhea
+    path("clubs/ctf", TemplateView.as_view(template_name="front_end/ctf.html"), name='ctf'), # Marina
 
     path("teams/", TemplateView.as_view(template_name="front_end/teams.html"), name='teams'),
-    path("teams/ccdc", TemplateView.as_view(template_name="front_end/ccdc.html"), name='ccdc'),
-    path("teams/cptc", TemplateView.as_view(template_name="front_end/cptc.html"), name='cptc'),
-    path("teams/ncae", TemplateView.as_view(template_name="front_end/ncae.html"), name='ncae'),
+    path("teams/ccdc", TemplateView.as_view(template_name="front_end/ccdc.html"), name='ccdc'), # Ryan
+    path("teams/cptc", TemplateView.as_view(template_name="front_end/cptc.html"), name='cptc'), # Areeb
+    path("teams/ncae", TemplateView.as_view(template_name="front_end/ncae.html"), name='ncae'), # Alec
     path("teams/cyberforce", TemplateView.as_view(template_name="front_end/cyberforce.html"), name='cyberforce'),
 ]
