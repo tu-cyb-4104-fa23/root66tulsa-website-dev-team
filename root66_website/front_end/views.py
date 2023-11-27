0b527a219b278d7158e62215a684de4bf18ea474
waterFallss <readerleader2017@gmail.com>
2023-11-27 09:09:19 -0600

    Added Branch Instruction

--- root66_website/front_end/views.py
+++ root66_website/front_end/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def home(request):
     """Home Page for Website"""
     return render(request, "front_end/home.html")
+
+def ctf(request):
+    "CTF Page for Website"
+    return render(request, "front_end/ctf.html")
